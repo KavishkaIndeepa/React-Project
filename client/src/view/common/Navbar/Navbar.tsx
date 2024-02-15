1f6c0eb8776a1e42cebf74219e9bb2f62aff1c58
Kavishka Indeepa <101946925+KavishkaIndeepa@users.noreply.github.com>
2024-02-16 00:32:36 +0530

    Update Home

--- client/src/view/common/Navbar/Navbar.tsx
+++ client/src/view/common/Navbar/Navbar.tsx
@@ -1,6 +1,4 @@
 import React, {Component} from "react";
-// @ts-ignore
-import logo from '../../../assets/Shoes_Logo__1.png';
 import {Link} from "react-router-dom";
 
 export class Navbar extends Component {
@@ -10,27 +8,30 @@ export class Navbar extends Component {
                 <div className="flex">
                     <h1 className="text-2xl text-Primary">Summer Shoes</h1>
 
-                    <img className="h-10 w-10 ml-1" src={logo} alt=""/>
                 </div>
 
-                <ul className="list-none mt-2 flex">
-                    <li className="mr-2 text-[18px] text-[#e6f0e6] hover:text-green-500">
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li className="mr-2 text-[18px] text-[#e6f0e6] hover:text-green-500">
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li className="mr-2 text-[18px] text-[#e6f0e6] hover:text-green-500">
-                        <Link to="/item">Items</Link>
-                    </li>
-                    <li className="mr-2 text-[18px] text-[#e6f0e6] hover:text-green-600">
-                        <Link to="/shopping-cart">My-Cart</Link>
-                    </li>
-                    <li className="mr-2 text-[18px] text-[#e6f0e6] hover:text-green-500">
-                        <Link to="/contact">Contact</Link>
-                    </li>
+                <div className={"flex"}>
 
-                </ul>
+                    <ul className="list-none mt-2 flex">
+                        <li className="mr-2 text-[18px] text-[#e6f0e6] hover:text-green-500">
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li className="mr-2 text-[18px] text-[#e6f0e6] hover:text-green-500">
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li className="mr-2 text-[18px] text-[#e6f0e6] hover:text-green-500">
+                            <Link to="/item">Items</Link>
+                        </li>
+                        <li className="mr-2 text-[18px] text-[#e6f0e6] hover:text-green-600">
+                            <Link to="/shopping-cart">My-Cart</Link>
+                        </li>
+                        <li className="mr-2 text-[18px] text-[#e6f0e6] hover:text-green-500">
+                            <Link to="/contact">Contact</Link>
+                        </li>
+
+                    </ul>
+
+                </div>
 
                 <div className="flex flex-col md:flex-row space-y-2.5 md:space-y-0 md:space-x-2.5">
 
--- client/src/view/pages/Home/Home.tsx
+++ client/src/view/pages/Home/Home.tsx
@@ -10,16 +10,28 @@ export class Home extends Component {
 
         return (
 
-            <div className="flex flex-wrap min-h-screen bg-cover"
-                 style={{backgroundImage: 'url("/Shoe-Rack-Img.png")'}}>
+            <div className={"flex-row flex-wrap "}>
+                <div className="flex flex-wrap min-h-screen bg-cover"
+                     style={{backgroundImage: 'url("/Shoe-Rack-Img.png")'}}>
 
-
-                <div className={"md:ml-16 md:mt-12"}>
-                    <h1 className={"p-2 mt-6 md:mt-12 ml-2 font-bold text-Primary text-4xl shadow-xl font-mono bg-gray-600 bg-opacity-40 md:rounded-lg"}>
-                        <span className={"text-Primary text-[64px]"}>A</span>re you ready to <br/> <span
-                        className={"text-white font-bold ml-20"}>
+                    <div className={"flex-col justify-center items-center"}>
+                        <h1 className={"font-bold text-Primary text-4xl shadow-xl font-mono bg-gray-600 bg-opacity-40 md:rounded-lg"}>
+                            <span className={"text-Primary text-[64px]"}>A</span>re you ready to <span
+                            className={"text-white font-bold "}>
                         Lead the way? </span></h1>
+                    </div>
+
+                    <div className={"flex-row flex-wrap justify-center pt-3"}>
+                        <Link className={"text-white flex items-center"} to="/item">
+                            <button className={"w-full ml-16 p-2 bg-black text-white animate-bounce"}>Buy Now
+                                <FontAwesomeIcon icon={faShoppingCart} className="ml-2"/></button>
+                        </Link>
+                    </div>
+
+
                 </div>
+
+
 
                 <div className={"w-full md:pl-4 md:pr-4"}>
                     <h1 className={"p-2 md:ml-8 md:mr-14 text-xl md:text-2xl text-white bg-black bg-opacity-40 md:rounded-lg"}>
@@ -30,16 +42,9 @@ export class Home extends Component {
                         and step confidently into the season with our diverse range of shoes. Quality, comfort, and
                         style—your journey to fabulous footwear starts here.</h1>
                 </div>
-
-                <div className={"pt-3"}>
-                    <Link className={"text-white flex items-center"} to="/item">
-                        <button className={"w-full ml-16 p-2 bg-black text-white animate-bounce"}>Buy Now
-                            <FontAwesomeIcon icon={faShoppingCart} className="ml-2"/></button>
-                    </Link>
-                </div>
-
-
             </div>
+
+
         );
     }
 }
